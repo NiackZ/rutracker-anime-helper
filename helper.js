a6cb3ffdb343a568bb17c8fb323849ef2174298d
NiackZ <niack@mail.ru>
2023-12-17 15:01:15 +0500

    Смена версии

--- helper.js
+++ helper.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         rutracker release helper
-// @version      1.1
+// @version      1.2
 // @description  Заполнение полей по данным со страницы аниме на сайте World-Art
 // @author       NiackZ
 // @match        https://rutracker.org/forum/posting.php?f=1105&mode=new_rel
