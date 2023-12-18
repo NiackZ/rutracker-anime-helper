0b8e15e2358ab54535d330ab99897a2f65b55427
NiackZ <niack@mail.ru>
2023-12-18 21:48:38 +0500

    MediaInfo парсер

--- helper.js
+++ helper.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         rutracker release helper
-// @version      1.9
+// @version      2.0
 // @description  Заполнение полей по данным со страницы аниме на сайте World-Art
 // @author       NiackZ
 // @match        https://rutracker.org/forum/posting.php?f=1105&mode=new_rel
@@ -31,20 +31,173 @@
             console.error('Ошибка: ', error.message);
         }
     };
-    const addRow = () => {
+    const MiParser = (miData) => {
+        const RU = 'RU';
+        const EN = 'EN';
+        const REGEX_EN = {
+            CODEC: /Format\s+:\s+([^\r\n]+)/,
+            BIT_RATE: /Bit rate\s+:\s+([^\r\n]+)/,
+            WIDTH: /Width\s+:\s+(\d+(?:\s+\d*)?)/,
+            HEIGHT: /Height\s+:\s+(\d+(?:\s+\d*)?)/,
+            ASPECT: /Display aspect ratio\s+:\s+([^\r\n]+)/,
+            FRAME_RATE: /Frame rate\s+:\s+([^\r\n]+)/,
+            BIT_DEPTH: /Bit depth\s+:\s+(\d+)/,
+            CHANNELS: /Channel\(s\)\s+:\s+(\d+)/,
+            SAMPLING_RATE: /Sampling rate\s+:\s+(.+)/,
+            LANGUAGE: /Language\s+:\s+(.+)/,
+            TITLE: /Title\s+:\s+(.+)/,
+            NAME: /Complete name\s+:\s+([^\r\n]+)/
+        };
+        const REGEX_RU = {
+            CODEC: /Формат\s+:\s+([^\r\n]+)/,
+            BIT_RATE: /Битрейт\s+:\s+([^\r\n]+)/,
+            WIDTH: /Ширина\s+:\s+(\d+(?:\s+\d*)?)/,
+            HEIGHT: /Высота\s+:\s+(\d+(?:\s+\d*)?)/,
+            ASPECT: /Соотношение сторон\s+:\s+([^\r\n]+)/,
+            FRAME_RATE: /Частота кадров\s+:\s+([^\r\n]+)/,
+            BIT_DEPTH: /Битовая глубина\s+:\s+(\d+)/,
+            CHANNELS: /Каналы\s+:\s+(\d+)/,
+            SAMPLING_RATE: /Частота\s+:\s+(.+)/,
+            LANGUAGE: /Язык\s+:\s+(.+)/,
+            TITLE: /Заголовок\s+:\s+(.+)/,
+            NAME: /Полное имя\s+:\s+([^\r\n]+)/
+        };
+
+        const parseField = (block, regex, fieldName) => {
+            const match = block.match(regex);
+            return match ? match[1].trim() : null;
+        };
+        const translateLanguage = (lang) => {
+            if (lang) {
+                switch (lang.toLowerCase()) {
+                    case "russian":
+                        return "Русский";
+                    case "japanese":
+                        return "Японский";
+                    case "english":
+                        return "Английский";
+                    default:
+                        return lang;
+                }
+            }
+            return null;
+        }
+        const getFileExt = (generalBlockMatch, lang) => {
+            if (generalBlockMatch) {
+                const generalBlock = generalBlockMatch[1].trim();
+                const fileName = parseField(generalBlock, lang === EN ? REGEX_EN.NAME: REGEX_RU.NAME);
+                return fileName.split('.').pop().toUpperCase();
+            }
+            return null;
+        };
+        const getVideoInfo = (videoBlockMatch, lang) => {
+            if (videoBlockMatch) {
+                const parseVideoBlock = (videoBlock, regex) => {
+                    const _width = parseField(videoBlock, regex.WIDTH);
+                    const _height = parseField(videoBlock, regex.HEIGHT);
+                    const _fps = parseField(videoBlock, regex.FRAME_RATE);
+
+                    return {
+                        codec: parseField(videoBlock, regex.CODEC),
+                        width: _width ? _width.replaceAll(" ", "") : null,
+                        height: _height ? _height.replaceAll(" ", "") : null,
+                        aspect: parseField(videoBlock, regex.ASPECT),
+                        fps: _fps ? `${_fps.split(" ")[0]} fps` : null,
+                        bitDepth: parseField(videoBlock, regex.BIT_DEPTH),
+                        bitRate: parseField(videoBlock, regex.BIT_RATE),
+                        fileExt: null
+                    };
+                }
+
+                return parseVideoBlock(videoBlockMatch[1].trim(), lang === EN ? REGEX_EN : REGEX_RU);
+            }
+
+            return null;
+        }
+        const getAudioInfo = (blocks, lang) => {
+            if (blocks) {
+                const parseAudioBlock = (blocks, regex) => {
+                    return Array.from(blocks).map(audioBlockMatch => {
+                        const audioBlock = audioBlockMatch[1].trim();
+                        return {
+                            language: translateLanguage(parseField(audioBlock, regex.LANGUAGE)),
+                            codec: parseField(audioBlock, regex.CODEC),
+                            bitRate: parseField(audioBlock, regex.BIT_RATE),
+                            sampleRate: parseField(audioBlock, regex.SAMPLING_RATE),
+                            channels: parseField(audioBlock, regex.CHANNELS),
+                            title: parseField(audioBlock, regex.TITLE)
+                        }
+                    });
+                }
+
+                return parseAudioBlock(blocks, lang === EN ? REGEX_EN : REGEX_RU);
+            }
+            return null;
+        }
+        const getTextInfo = (blocks, lang) => {
+            if (blocks) {
+                const parseTextBlock = (blocks, regex) => {
+                    return Array.from(blocks).map(textBlockMatch => {
+                        const textBlock = textBlockMatch[1].trim();
+                        return {
+                            language: translateLanguage(parseField(textBlock, regex.LANGUAGE)),
+                            format: parseField(textBlock, regex.CODEC),
+                            title: parseField(textBlock, regex.TITLE)
+                        }
+                    });
+                }
+
+                return parseTextBlock(blocks, lang === EN ? REGEX_EN : REGEX_RU);
+            }
+            return null;
+        }
+
+        const getRegexBlocks = {
+            VIDEO: /Video([\s\S]+?)(?=(\n\n|\r\n\r\n|$))/,
+            VIDEO_RU: /Видео([\s\S]+?)(?=(\n\n|\r\n\r\n|$))/,
+            AUDIO: /Audio([\s\S]+?)(?=(\n\n|\r\n\r\n|$))/g,
+            AUDIO_RU: /Аудио([\s\S]+?)(?=(\n\n|\r\n\r\n|$))/g,
+            TEXT: /Text([\s\S]+?)(?=(\n\n|\r\n\r\n|$))/g,
+            TEXT_RU: /Текст([\s\S]+?)(?=(\n\n|\r\n\r\n|$))/g,
+            GENERAL: /General([\s\S]+?)(?=(\n\n|\r\n\r\n|$))/,
+            GENERAL_RU: /Общее([\s\S]+?)(?=(\n\n|\r\n\r\n|$))/
+        }
+
+        const videoBlockMatch = miData.match(getRegexBlocks.VIDEO);
+        const audioBlockMatches = miData.matchAll(getRegexBlocks.AUDIO);
+        const textBlockMatches = miData.matchAll(getRegexBlocks.TEXT);
+        const generalBlockMatch = miData.match(getRegexBlocks.GENERAL);
+
+        const videoBlockMatch_RU = miData.match(getRegexBlocks.VIDEO_RU);
+        const audioBlockMatches_RU = miData.matchAll(getRegexBlocks.AUDIO_RU);
+        const textBlockMatches_RU = miData.matchAll(getRegexBlocks.TEXT_RU);
+        const generalBlockMatch_RU = miData.match(getRegexBlocks.GENERAL_RU);
+
+        const useEN = videoBlockMatch_RU === null &&
+            Array.from(audioBlockMatches_RU).length === 0 &&
+            Array.from(textBlockMatches_RU).length === 0 &&
+            generalBlockMatch_RU === null;
+
+        const lang = useEN ? EN : RU;
+
+        const videoInfo = getVideoInfo(useEN ? videoBlockMatch : videoBlockMatch_RU, lang);
+        const audioInfo = getAudioInfo(useEN ? audioBlockMatches : audioBlockMatches_RU, lang);
+        const textInfo = getTextInfo(useEN ? textBlockMatches : textBlockMatches_RU, lang);
+        videoInfo.fileExt = getFileExt(useEN ? generalBlockMatch : generalBlockMatch_RU, lang);
+
+        return { videoInfo, audioInfo, textInfo };
+    }
+    const addUrlRow = () => {
         const tbody = document.getElementById('rel-tpl');
         const newRow = tbody.insertRow(0);
 
-        // Создаем ячейку с классом rel-title
         const titleCell = newRow.insertCell(0);
         titleCell.className = 'rel-title';
         titleCell.innerText = 'Ссылка на аниме:';
 
-        // Создаем ячейку с классом rel-inputs
         const inputsCell = newRow.insertCell(1);
         inputsCell.className = 'rel-inputs';
 
-        // Создаем текстовое поле в ячейке с классом rel-input
         const textField = document.createElement('input');
         textField.className = 'rel-el rel-input';
         textField.type = 'text';
@@ -54,17 +207,14 @@
         textField.placeholder = '';
         inputsCell.appendChild(textField);
 
-        // Создаем span с классом rel-el rel-free-el
         const freeEl = document.createElement('span');
         freeEl.className = 'rel-el rel-free-el';
         freeEl.innerText = 'URL';
         inputsCell.appendChild(freeEl);
 
-        // Создаем span с классом rel-el
         const buttonSpan = document.createElement('span');
         buttonSpan.className = 'rel-el';
 
-        // Создаем кнопку "Заполнить" внутри span
         const fillButton = document.createElement('input');
         fillButton.type = 'button';
         fillButton.style.width = '100px';
@@ -91,11 +241,8 @@
                 fillButton.disabled = false;
             }
 
-
         };
         buttonSpan.appendChild(fillButton);
-
-        // Добавляем span с кнопкой в ячейку
         inputsCell.appendChild(buttonSpan);
 
         const br = document.createElement('br');
@@ -113,17 +260,16 @@
         link.target = '_blank';
 
         infoSpan.appendChild(link);
-
         inputsCell.appendChild(infoSpan);
     }
+    const setOptionIfExists = (select, value) => {
+        const optionExists = Array.from(select.options).some(option => option.value === value);
+        if (optionExists) {
+            select.value = value;
+        }
+    };
     const fillFields = (anime) => {
         console.log(anime);
-        const setOptionIfExists = (select, value) => {
-            const optionExists = Array.from(select.options).some(option => option.value === value);
-            if (optionExists) {
-                select.value = value;
-            }
-        };
         const rusName = document.getElementById("title_rus");
         const engName = document.getElementById("title_eng");
         const othName = document.getElementById("19196242af37f0ad77a7f593b45f8207");
@@ -209,5 +355,154 @@
         }
 
     }
-    addRow();
+    const getTechData = (miData) => {
+        const textareaElement = document.getElementById("60503004a43535a7eb84520612a2e26c");
+        return textareaElement.value ? MiParser(textareaElement.value) : null;
+    }
+    const addTechButton = () => {
+        const textareaElement = document.getElementById("60503004a43535a7eb84520612a2e26c");
+        if (textareaElement) {
+            const buttonSpan = document.createElement('span');
+            buttonSpan.className = 'rel-el';
+
+            const fillButton = document.createElement('input');
+            fillButton.type = 'button';
+            fillButton.style.width = '150px';
+            fillButton.value = 'Заполнить тех. данные';
+            fillButton.onclick = function() {
+                try{
+                    const techData = getTechData();
+                    console.log(techData);
+                    if (techData) {
+                        const videoFormat = document.getElementById("video_format");
+                        const HDFormat = document.getElementById("7dc5360181c073093213c3a33682c1ea");
+                        const video = document.getElementById("video");
+                        const audioFields = [
+                            {
+                                audio: document.getElementById("390c478dabbdfa0a78d0f7a1e69d4dfa"),
+                                lang: document.getElementById("lang_anime"),
+                                title: document.getElementById("3890534b0fe393e9ff6490d7160b000d")
+                            },
+                            {
+                                audio: document.getElementById("8f8407c22d6ee07cdee27ef409fc7fb1"),
+                                lang: document.getElementById("lang_anime_2"),
+                                title: document.getElementById("59bbcdb0c6493c4faf8a92e7c6345004")
+                            },
+                            {
+                                audio: document.getElementById("bb467dadf3966b8bed32a178e398bdbb"),
+                                lang: document.getElementById("lang_anime_3"),
+                                title: document.getElementById("5c5af4d271c2bcdba51db721004b8de2")
+                            }
+                        ]
+                        const textField = [
+                            {
+                                text: document.getElementById("38f8b090c53af5748bfc06686db808f6"),
+                                lang: document.getElementById("sub_all_anime"),
+                                title: document.getElementById("ea9559bbb5520f92f25309d8194035e2")
+                            },
+                            {
+                                text: document.getElementById("0bc605b1fd444b189e35ee70aa5ca5ac"),
+                                lang: document.getElementById("sub_all_anime_2"),
+                                title: document.getElementById("7f34a9e3be76d5f4dfc7fd27bc2f745b")
+                            },
+                            {
+                                text: document.getElementById("0736e6092bce471f377fe38e33eac723"),
+                                lang: document.getElementById("sub_all_anime_3"),
+                                title: document.getElementById("112f186c9190a9cf4d95c151d3a3fe42")
+                            }
+                        ]
+                        if (techData.videoInfo) {
+                            if (techData.videoInfo?.fileExt) {
+                                setOptionIfExists(videoFormat, techData.videoInfo.fileExt);
+                            }
+                            if (techData.videoInfo?.height) {
+                                setOptionIfExists(HDFormat, `${techData.videoInfo?.height}p`);
+                            }
+                            const videoInfo = [];
+                            if (techData.videoInfo?.codec) {
+                                videoInfo.push(techData.videoInfo.codec);
+                            }
+                            if (techData.videoInfo?.width && techData.videoInfo?.height && techData.videoInfo?.aspect) {
+                                videoInfo.push(`${techData.videoInfo.width}x${techData.videoInfo.height} (${techData.videoInfo.aspect})`);
+                            }
+                            if (techData.videoInfo?.bitRate) {
+                                videoInfo.push(techData.videoInfo.bitRate);
+                            }
+                            if (techData.videoInfo?.fps) {
+                                videoInfo.push(techData.videoInfo.fps);
+                            }
+                            if (techData.videoInfo?.bitDepth) {
+                                videoInfo.push(`${techData.videoInfo.bitDepth}bit`);
+                            }
+                            video.value = videoInfo.join(', ');
+                        }
+                        if (techData.audioInfo) {
+                            for (let i = 0; i < techData.audioInfo.length; i++) {
+                                const audio = techData.audioInfo[i];
+                                const audioBlock = audioFields[i];
+                                if (!audioBlock) break;
+                                const audioInfo = [];
+                                if (audio?.language) {
+                                    audioInfo.push(audio.language);
+                                }
+                                if (audio?.codec) {
+                                    audioInfo.push(audio.codec);
+                                }
+                                if (audio?.bitRate) {
+                                    audioInfo.push(audio.bitRate);
+                                }
+                                if (audio?.sampleRate) {
+                                    audioInfo.push(audio.sampleRate);
+                                }
+                                if (audio?.channels) {
+                                    audioInfo.push(`${audio.channels} канала`);
+                                }
+                                audioBlock.audio.value = audioInfo.join(', ');
+                                setOptionIfExists(audioBlock.lang, audio.language === "Русский"
+                                    ? 'Русский (в составе контейнера)'
+                                    : audio.language
+                                );
+                                if (audio?.title) {
+                                    audioBlock.title.value = audio.title;
+                                }
+                            }
+                        }
+                        if (techData.textInfo) {
+                            for (let i = 0; i < techData.textInfo.length; i++) {
+                                const text = techData.textInfo[i];
+                                const textBlock = textField[i];
+                                if (!textBlock) break;
+                                //Русский, ASS, Полные + Надписи - Crunchyroll (в составе контейнера)
+                                const textInfo = [];
+                                if (text?.language) {
+                                    textInfo.push(text.language);
+                                }
+                                if (text?.format) {
+                                    textInfo.push(text.format);
+                                }
+                                textInfo.push("встроенные");
+                                textBlock.text.value = textInfo.join(", ");
+                                setOptionIfExists(textBlock.lang, text.language?.toLowerCase());
+                                if (text?.title) {
+                                    textBlock.title.value = text.title;
+                                }
+                            }
+                        }
+
+                    }
+                }
+                catch (error) {
+                    console.error('Ошибка: ', error.message);
+                }
+            };
+            buttonSpan.appendChild(fillButton);
+            textareaElement.parentNode.appendChild(buttonSpan);
+
+        } else {
+            console.error("Textarea не найден по указанному идентификатору.");
+        }
+    }
+
+    addUrlRow();
+    addTechButton();
 })();
